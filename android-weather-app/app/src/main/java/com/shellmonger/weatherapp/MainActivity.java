55516816087d0500c24d86bbc7342a7df24d0188
Adrian Hall <adrian@shellmonger.com>
2017-06-03 11:47:36 -0700

    Added UI

--- android-weather-app/app/src/main/java/com/shellmonger/weatherapp/MainActivity.java
+++ android-weather-app/app/src/main/java/com/shellmonger/weatherapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.shellmonger.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView weatherIcon;
+    Typeface weatherFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        weatherIcon = (TextView)findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
     }
 }
